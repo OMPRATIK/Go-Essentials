2ba59fa4358a5fb8c05a9e5df5cd040060c9fbe7
OMPRATIK <routprateek4@gmail.com>
2024-09-11 01:48:40 +0530

    commplete arrays, slice, maps, make, for range loop

---
+++ arrays/maps/maps.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"Google":              "google.com",
+		"Amazon Web Services": "www.aws.com",
+	}
+	fmt.Println(websites)
+	fmt.Println(websites["Google"])
+	
+	// add new key-value pair in map
+	websites["Linkedin"] = "https://linkedin.com"
+	fmt.Println(websites)
+	
+	// delete key-value from map
+	
+	delete(websites, "Google")
+	fmt.Println(websites)
+
+
+}
