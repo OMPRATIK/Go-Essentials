bf1accfc06a1babada1d59a8db54ef92f8690bf8
OMPRATIK <routprateek4@gmail.com>
2025-04-06 23:52:24 +0530

    conc

--- concurrency/orders.go
+++ concurrency/orders.go
@@ -1,15 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 type Order struct {
 	ID     int
 	Status string
 }
 
-func GenerateOrders(count int) []Order {
-	orders := make([]Order, count)
+func ProcessOrders(orders []*Order) {
+	for _, order := range orders {
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		fmt.Printf("Processing order %d\n", order.ID)
+	}
+}
+
+func GenerateOrders(count int) []*Order {
+	orders := make([]*Order, count)
 
 	for idx := range orders {
-		orders[idx] = Order{
+		orders[idx] = &Order{
 			ID:     idx + 1,
 			Status: "Pending",
 		}
